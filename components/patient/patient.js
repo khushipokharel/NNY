faa6a1d08c8a2c4f4dc10eaf1aa265425cbd8e21
unknown <pham0379@flinders.edu.au>
2024-08-29 22:17:06 +0930

    Updated header and footer template for patient and therapist pages

---
+++ components/patient/patient.js
@@ -0,0 +1,17 @@
+function loadHeader() {
+    fetch('../components/shared/patient/header.html')
+         .then(response => response.text())
+         .then(data => {
+            document.getElementById('header').innerHTML = data;
+         });
+}
+loadHeader();
+
+function loadfooter() {
+    fetch('../components/shared/patient/footer.html')
+         .then(response => response.text())
+         .then(data => {
+            document.getElementById('footer').innerHTML = data;
+         });
+}
+loadfooter();
